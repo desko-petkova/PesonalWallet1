9b5fa8283ddcee3fde2aee72069efe82c565f9fa
PCK216 <PCK216@K216>
2026-03-09 14:08:53 +0200

    WalletStorage and FileStorage implemented

---
+++ PesonalWallet1/Infrastucture/FileStorage.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+namespace PesonalWallet1.Infrastucture
+{
+    public class FileStorage
+    {
+        private readonly string path;
+
+        public FileStorage(string path = "wallet.json")
+        {
+            this.path = path;
+        }
+        public WalletStorage Load()
+        {
+            if (!File.Exists(path))
+            {
+                return new WalletStorage();
+            }
+
+            var json = File.ReadAllText(path);
+
+            var storage = JsonSerializer.Deserialize<WalletStorage>(json);
+
+            return storage;
+
+        }
+        public void Save(WalletStorage storage)
+        {
+            var json = JsonSerializer.Serialize(storage);
+            File.WriteAllText(path, json);
+
+        }
+
+    }
+}
---
+++ PesonalWallet1/Infrastucture/WalletStorage.cs
@@ -0,0 +1,12 @@
+﻿using PesonalWallet1.Domain.Entities;
+
+namespace PesonalWallet1.Infrastucture
+{
+    public class WalletStorage
+    {
+        public List<Account> Accounts { get; set; } = new List<Account>();
+
+        public List<Transation> Transactions { get; set; } = new List<Transation>();
+
+    }
+}
