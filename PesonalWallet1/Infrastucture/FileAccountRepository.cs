cf68c21b12e9c142331c263a2fbdaecc8b9bb18b
PCK216 <PCK216@K216>
2026-03-09 15:07:37 +0200

    FileAccountRepository added and methods Save, GEtAll and GetById implemented

---
+++ PesonalWallet1/Infrastucture/FileAccountRepository.cs
@@ -0,0 +1,77 @@
+﻿using PesonalWallet1.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesonalWallet1.Infrastucture
+{
+    public class FileAccountRepository
+    {
+        private readonly FileStorage storage;
+
+        public FileAccountRepository(FileStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public IReadOnlyList<Account> GetAll()
+        {
+            var db = storage.Load();
+            return db.Accounts;
+        }
+
+        public Account GetById(int id)
+        {
+            var db = storage.Load();
+
+            foreach (var account in db.Accounts)
+            {
+                if (account.Id == id)
+                {
+                    return account;
+                }
+            }
+
+            throw new Exception("Account not found");
+        }
+
+        public void Save(Account account)
+        {
+            var db = storage.Load();
+
+            if(account.Id == 0)
+            {
+                var newAccount = new Account(
+                    //int id, string name, AccountType type, Money balance
+                    db.NextId++,
+                    account.Name,
+                    account.Type,
+                    account.Balance
+                    );
+                db.Accounts.Add(newAccount);
+            }
+            else
+            {
+                bool found = false;
+                for (int i = 0; i < db.Accounts.Count; i++)
+                {
+                    if (db.Accounts[i].Id == account.Id)
+                    {
+                        db.Accounts[i] = account;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    throw new Exception("Account not found");
+                }
+            }
+
+            storage.Save(db);
+
+        }
+    }
+}
--- PesonalWallet1/Infrastucture/WalletStorage.cs
+++ PesonalWallet1/Infrastucture/WalletStorage.cs
@@ -4,6 +4,7 @@ namespace PesonalWallet1.Infrastucture
 {
     public class WalletStorage
     {
+        public int NextId { get; set; } = 1;
         public List<Account> Accounts { get; set; } = new List<Account>();
 
         public List<Transation> Transactions { get; set; } = new List<Transation>();
