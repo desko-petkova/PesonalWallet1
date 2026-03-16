8048e41c1d15d34632bbdd1eb40ef9248284ff43
PCK216 <PCK216@K216>
2026-03-16 13:20:49 +0200

    GetByAccountId mothod added

--- PesonalWallet1/Infrastucture/FileTransactionRepository.cs
+++ PesonalWallet1/Infrastucture/FileTransactionRepository.cs
@@ -1,13 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PesonalWallet1.Domain.Entities;
 
 namespace PesonalWallet1.Infrastucture
 {
     public class FileTransactionRepository
     {
+        private readonly FileStorage storage;
 
+        public FileTransactionRepository(FileStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public void Save(Transation transaction)
+        {
+            var db = storage.Load();
+            var newTransation = new Transation(
+                    db.NextId++,
+                    transaction.AccountId,
+                    transaction.Type,
+                    transaction.Amount,
+                    transaction.Date
+                    );
+            db.Transactions.Add(newTransation);
+
+            storage.Save(db);
+        }
+
+        public IReadOnlyList<Transation> GetByAccountId(int id)
+        {
+            var db = storage.Load();
+
+            var result = new List<PesonalWallet1.Domain.Entities.Transation>();
+
+            foreach (var transaction in db.Transactions)
+            {
+                if (transaction.AccountId == id)
+                    result.Add(transaction);
+            }
+            return result;
+        }
     }
 }
