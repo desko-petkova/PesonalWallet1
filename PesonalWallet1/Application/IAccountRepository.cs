504fd28b33ec308cd6ea6391446c569447b12480
PCK216 <PCK216@K216>
2026-03-16 14:00:09 +0200

    Repository Interfaces added

---
+++ PesonalWallet1/Application/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using PesonalWallet1.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesonalWallet1.Application
+{
+    public interface IAccountRepository
+    {
+        IReadOnlyList<Account> GetAll();
+        Account GetById(int id);
+        void Save(Account account);
+    }
+}
---
+++ PesonalWallet1/Application/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using PesonalWallet1.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesonalWallet1.Application
+{
+    public interface ITransactionRepository
+    {
+        IReadOnlyList<Transation> GetByAccountId(int id);
+        void Save(Transation transaction);
+    }
+}
